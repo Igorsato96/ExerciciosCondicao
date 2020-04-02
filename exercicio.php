69c833b594af248db2ba8df9f1a6479a9600aba4
Igorsato96 <62575213+Igorsato96@users.noreply.github.com>
2020-04-02 19:27:50 -0300

    Create exercicio.php

---
+++ exercicio.php
@@ -0,0 +1,76 @@
+<?php
+// Exercicio 1
+   $i = 9;
+if($i>=0 && $i<=9){
+    echo "Esta dentro";
+}else{
+    echo"Nao esta dentro";
+}
+
+// Exercicio 2
+// $i=-1;
+// if($i>=0 && $i<=100){
+//     echo "esta entre 0 e 100";
+// }elseif($i<0){
+//     echo"é negativo";
+// }else{
+//     echo "é maior que 100";
+// }
+
+// "Exercicio 3";
+// $i=1;
+// if($i%2==0){
+//     echo"par";
+// }else{
+//     echo"impar";
+// }
+
+// Exercicio 4
+// $a =10;
+// $b =2;
+// $c =80;
+// $d =444;
+// $e =50;
+// $menor =$a;
+// if($b<$a and $b<$c and $b<$d and $b<$e){
+//     $menor=$b;
+// }
+// if($c<$a and $c<$b and $c<$d and $c<$e){
+//     $menor=$c;
+// }
+// if($d<$a and $d<$b and $d<$c and $d<$e){
+//     $menor=$d;
+// }
+// if($e<$a and $e<$b and $e<$c and $e<$d){
+//     $menor=$e;
+// }
+// echo $menor;
+// $maior =$a;
+// if($b>$a and $b>$c and $b>$d and $b>$e){
+//     $maior=$b;
+// }
+// if($c>$a and $c>$b and $c>$d and $c>$e){
+//     $maior=$c;
+// }
+// if($d>$a and $d>$b and $d>$c and $d>$e){
+//     $maior=$d;
+// }
+// if($e>$a and $e>$b and $e>$c and $e>$d){
+//     $maior=$e;
+// }
+// echo $maior;
+
+// Exercicio 5
+// $a=6;
+// $b=5;
+// $c=4;
+// if($a<0 || $b<0 || $c<0){
+//     echo "nao e possivel criar o triangulo";
+// }elseif($a==$b && $a==$c && $b==$c ){
+//     echo "trinagulo equilatero que possui 3 lados iguais";
+// }elseif($a==$b || $a==$c || $b==$c){
+//     echo "triangulo isosceles que possui 2 lados iguais";
+// }elseif($a!=$b || $a!=$c || $b!=$c){
+//     echo "triangulo escaleno que nao possui lados iguais";
+// }
+?>
